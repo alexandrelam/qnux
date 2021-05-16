1e6b94318bb17953a4877332de40b23009059e6e
Alexandre LAM <alexandrelam@outlook.com>
2021-05-16 16:08:12 +0200

    fix remove element from list flickering

--- components/ListElement.vue
+++ components/ListElement.vue
@@ -2,8 +2,8 @@
   div(@mouseover="isHover=true" @mouseleave="isHover=fasle").flex.gap-4.items-center
     input(type="checkbox" v-model="isChecked" @change="$emit('check', isChecked)")
     input(type="text" v-model="textvalue").focus_outline-none
-    div(v-show="isHover")
-      svg(xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor")
+    div.h-5.w-5
+      svg(v-show="isHover" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor")
         path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12")
 
 </template>
